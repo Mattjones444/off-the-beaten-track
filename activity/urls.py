d358e957a57d7cd1c37b59ba3a3a5647f3133267
Matt Jones <mattjones444@gmail.com>
2024-05-22 21:50:43 +0000

    added product detail page

--- activity/urls.py
+++ activity/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_activity, name='activity'),
+    path('<activity_id>', views.activity_detail, name='activity_detail'),
 ]
--- activity/views.py
+++ activity/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category,Activity
 
 def all_activity(request):
@@ -12,3 +12,15 @@ def all_activity(request):
 
 
     return render(request, 'activity/activity.html', context)
+
+
+def activity_detail(request, activity_id):
+    """ A view to show individual activity details """
+
+    activity = get_object_or_404(Activity, pk=activity_id)
+    
+    context = {
+        'activity': activity,
+    }
+
+    return render(request, 'activity/activity_detail.html', context)

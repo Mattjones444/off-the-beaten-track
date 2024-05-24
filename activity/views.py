8af4708471a762c9ad18e1d42a7256e80eee4132
Matt Jones <mattjones444@gmail.com>
2024-05-24 16:14:47 +0000

    added main nav bar search function

--- activity/views.py
+++ activity/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Category,Activity
+
 
 def all_activity(request):
     """ A view to return the activity page """
 
     activity = Activity.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('activity'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            activity = activity.filter(queries)
+
+            print(activity)
 
     context = {
         "activity": activity,
+        'search_term': query,
     }
 
 

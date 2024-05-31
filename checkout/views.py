22936e85ae9e8df22840b7a2fc9137508972e334
Matt Jones <mattjones444@gmail.com>
2024-05-31 14:40:17 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PMWHf1X468Q47vIHuiWTLAmOYLwwi9UEVuFoe0pHVtyK6U3yoplpv0gthjc4Mxqvy0KLZAgLn6mBIgorid79zy400SmaWflFf',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

c62d003a11fe77a37017e690e7a498182af9792b
Matt Jones <mattjones444@gmail.com>
2024-06-23 14:32:18 +0000

    deployment steps added

--- beaten_track/settings.py
+++ beaten_track/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['Mattjones444 off-the-beaten-track.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mattjones444 off-the-beaten-track.herokuapp.com', 'localhost']
 
 
 # Application definition

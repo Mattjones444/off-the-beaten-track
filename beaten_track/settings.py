845d3ba619a50aeb0d649cb08b464cc1233f5d0c
Matthew Jones <mattjones444@gmail.com>
2024-06-24 16:26:04 +0000

    updated email confirmation settings

--- beaten_track/settings.py
+++ beaten_track/settings.py
@@ -223,5 +223,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 

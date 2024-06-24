5dc7c8f2cacdf496b7cbe9af0e3c3686234ebc41
Matthew Jones <mattjones444@gmail.com>
2024-06-24 16:16:14 +0000

    added email confirmation settings

--- beaten_track/settings.py
+++ beaten_track/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['off-the-beaten-track-b019d3277c26.herokuapp.com', 'localhost', 'www.beaten_track.herokuapp.com', '8000-offthebeate-pl58ipzsf8f.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['off-the-beaten-track-b019d3277c26.herokuapp.com', 'localhost', 'www.beaten_track.herokuapp.com',]
 
 
 
@@ -210,5 +210,18 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'beaten-track@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
